1f7622a7ba2fb26dc7c36672a812069aabf897d1
Unknown <sjain@student.fullsail.edu>
2018-07-23 19:07:08 -0400

    Marston animations implemented
    Working on animator and player controller script.

---
+++ Project Show Case/Assets/Scripts/PlayerControllerScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+    SpriteRenderer mySpriteRenderer;
+
+    [SerializeField]
+    float speed;
+
+    bool isShooting;
+
+
+    private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+        SetAnimator();
+    }
+
+
+    private void PlayerInput()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                mySpriteRenderer.flipX = false;
+            }
+            else if (Input.GetAxis("Horizontal") < 0)
+            {
+                mySpriteRenderer.flipX = true;
+            }
+
+            myRigidbody.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), myRigidbody.velocity.y);
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            isShooting=true;
+        }
+        else
+        {
+            isShooting = false;
+        }
+    }
+
+    private void SetAnimator()
+    {
+        if (Mathf.Abs(myRigidbody.velocity.x) > 0 && Input.GetAxis("Horizontal") != 0)
+        {
+            myAnimator.SetBool("Running", true);
+        }
+        else
+        {
+            myAnimator.SetBool("Running", false);
+        }
+
+        myAnimator.SetBool("Shooting", isShooting);
+
+    }
+}
