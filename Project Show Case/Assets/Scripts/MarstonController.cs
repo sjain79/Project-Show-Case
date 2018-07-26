bde731bb1d43a82655f27f6991f34d3d2c81a9a5
Unknown <sjain@student.fullsail.edu>
2018-07-26 03:35:12 -0400

    Reworked attack animation
    Started with shooting logic.
    Made bullet prefab.
    Changed speeds and names of animation fields.

--- Project Show Case/Assets/Scripts/MarstonController.cs
+++ Project Show Case/Assets/Scripts/MarstonController.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerScript : MonoBehaviour
+public class MarstonController : MonoBehaviour
 {
     Rigidbody2D myRigidbody;
     Animator myAnimator;
     SpriteRenderer mySpriteRenderer;
+
+    GameObject spawnPosition;
 
     [SerializeField]
     float speed;
@@ -14,11 +16,14 @@ public class PlayerControllerScript : MonoBehaviour
     [SerializeField]
     string playerNumber;
 
+    [SerializeField]
+    GameObject bullet;
+
     bool isShooting;
     bool isFalling;
     bool isTouchingGround;
     bool isJumping;
-    bool isAttacking1, isAttacking2, isAttacking3;
+    bool isAttacking;
 
     bool isDead;
 
@@ -27,6 +32,10 @@ public class PlayerControllerScript : MonoBehaviour
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
+
+        spawnPosition = transform.GetChild(0).gameObject;
+        //spawnPosition.transform.position = new Vector2(0.75f, 0);
+
         isDead = false;
     }
 
@@ -49,16 +58,18 @@ public class PlayerControllerScript : MonoBehaviour
             if (Input.GetAxis("Player " + playerNumber + " Horizontal") > 0)
             {
                 mySpriteRenderer.flipX = false;
+                spawnPosition.transform.localPosition = new Vector2((Mathf.Abs(spawnPosition.transform.localPosition.x)), (spawnPosition.transform.localPosition.y));
             }
             else if (Input.GetAxis("Player " + playerNumber + " Horizontal") < 0)
             {
                 mySpriteRenderer.flipX = true;
+                spawnPosition.transform.localPosition = new Vector2(-(Mathf.Abs(spawnPosition.transform.localPosition.x)), (spawnPosition.transform.localPosition.y));
             }
 
             myRigidbody.velocity = new Vector2(speed * Input.GetAxis("Player " + playerNumber + " Horizontal"), myRigidbody.velocity.y);
         }
 
-        if (Input.GetButton("Player "+playerNumber+" Fire 1"))
+        if (Input.GetButton("Player " + playerNumber + " Fire 1"))
         {
             isShooting = true;
         }
@@ -74,17 +85,17 @@ public class PlayerControllerScript : MonoBehaviour
             isJumping = true;
         }
 
-        if (Input.GetButtonDown("Player " + playerNumber + " Fire 4"))
+        //if (Input.GetButtonDown("Player " + playerNumber + " Fire 4"))
+        //{
+        //    isAttacking3 = true;
+        //}
+        //else if (Input.GetButtonDown("Player " + playerNumber + " Fire 3"))
+        //{
+        //    isAttacking2 = true;
+        //}
+        if (Input.GetButtonDown("Player " + playerNumber + " Fire 2"))
         {
-            isAttacking3 = true;
-        }
-        else if (Input.GetButtonDown("Player " + playerNumber + " Fire 3"))
-        {
-            isAttacking2 = true;
-        }
-        else if (Input.GetButtonDown("Player " + playerNumber + " Fire 2"))
-        {
-            isAttacking1 = true;
+            isAttacking = true;
         }
 
     }
@@ -119,11 +130,11 @@ public class PlayerControllerScript : MonoBehaviour
 
         myAnimator.SetBool("Dead", isDead);
 
-        myAnimator.SetBool("Attack 3", isAttacking3);
+        //myAnimator.SetBool("Attack 3", isAttacking3);
 
-        myAnimator.SetBool("Attack 2", isAttacking2);
+        //myAnimator.SetBool("Attack 2", isAttacking2);
 
-        myAnimator.SetBool("Attack 1", isAttacking1);
+        myAnimator.SetBool("Attack", isAttacking);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -142,20 +153,28 @@ public class PlayerControllerScript : MonoBehaviour
 
     public void CompleteAttack(int attackBoolean)
     {
-        switch (attackBoolean)
-        {
-            case 1:
-                isAttacking1 = false;
-                break;
-            case 2:
-                isAttacking2 = false;
-                break;
-            case 3:
-                isAttacking3 = false;
-                break;
-            default:
-                Debug.LogError("Switch out of bound!");
-                break;
-        }
+        //switch (attackBoolean)
+        //{
+        //    case 1:
+        //        isAttacking1 = false;
+        //        break;
+        //    case 2:
+        //        isAttacking2 = false;
+        //        break;
+        //    case 3:
+        //        isAttacking3 = false;
+        //        break;
+        //    default:
+        //        Debug.LogError("Switch out of bound!");
+        //        break;
+        //}
+
+        isAttacking = false;
+    }
+
+    public void ShootBullet()
+    {
+        Debug.Log("Function Called");
+        Instantiate(bullet, spawnPosition.transform.position, Quaternion.Euler(0, 0, mySpriteRenderer.flipX ? 180 : 0));
     }
 }
