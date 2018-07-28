eeb97e879a55264e37b13a6be596b710d5540a61
Unknown <sjain@student.fullsail.edu>
2018-07-28 18:49:56 -0400

    Added TextMesh Pro to the project

---
+++ Project Show Case/Assets/ControllerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ControllerInput : MonoBehaviour
+{
+    TextMeshProUGUI side1, side2;
+
+    private void Update()
+    {
+        Debug.Log(Input.GetJoystickNames());
+        if (Input.GetButtonDown())
+        {
+            Debug.Log("Controllrt Start Button Pressed");
+        }
+    }
+
+}
