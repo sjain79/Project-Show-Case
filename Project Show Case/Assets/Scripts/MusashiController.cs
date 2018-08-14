2f5231cde0686a3cf9687b0df5dd9299f0701bbb
Juan <juandiegoalzatemejia@gmail.com>
2018-08-14 18:06:09 -0400

    Added the MusashiController Script

---
+++ Project Show Case/Assets/Scripts/MusashiController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusashiController : CharacterScript {
+
+    bool isSlashing;
+    bool isAttacking1, isAttacking2, isAttacking3;
+
+    private void Awake()
+    {
+
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        PlayerInput();
+        SetAnimator();
+    }
+
+
+    private void PlayerInput()
+    {
+        if (isDead)
+            return;
+
+        if (Input.GetButton("Player " + playerNumber + " Fire 1") && !isJumping && !isFalling)
+        {
+            isSlashing = true;
+        }
+
+        //TEMPORAL
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            TakeDamage();
+        }
+
+        //TEMPORAL
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Die();
+        }
+
+    }
+
+    private void SetAnimator()
+    {
+        myAnimator.SetBool("Special", isSlashing);
+    }
+}
